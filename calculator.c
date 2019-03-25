86f408633d7509bd261f6a13717340f8aa2f3f3e
Carlos Lantigua <clantigua954@gmail.com>
2019-03-25 09:29:29 -0400

    fixed bug with continue input, fixed division

--- calculator.c
+++ calculator.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
-// #include <conio.h>
+#include "getch.c"
 
 int main()
 {
     int n, num1, num2, result;
-    printf("\nwhat operation do you want to do?\n");
-    printf("press 1 for addition\n");
-    printf("press 2 for subtraction\n");
-    printf("press 3 for multiplication\n");
-    printf("press 4 for division\n");
-    scanf("%d", &n);
-    printf("please enter a number\n");
-    scanf("%d", &num1);
-    printf("please enter a second number\n");
-    scanf("%d", &num2);
-    switch (n)
+    char option;
+    do
     {
-    case 1:
-        result = num1 + num2;
-        printf("addition of two numbers is %d\n", result);
-        break;
-    case 2:
-        result = num1 - num2;
-        printf("subtraction of two numbers is %d\n", result);
-        break;
-    case 3:
-        result = num1 * num2;
-        printf("multiplication of two numbers is %d\n", result);
-        break;
-    case 4:
-        result = num1 * num2;
-        printf("division of two numbers is %d\n", result);
-        break;
 
-    default:
-        printf("wrong input\n");
-        break;
-    }
-
+        printf("\nwhat operation do you want to do?\n");
+        printf("press 1 for addition\n");
+        printf("press 2 for subtraction\n");
+        printf("press 3 for multiplication\n");
+        printf("press 4 for division\n");
+        scanf("%d", &n);
+        printf("please enter a number\n");
+        scanf("%d", &num1);
+        printf("please enter a second number\n");
+        scanf("%d", &num2);
+        switch (n)
+        {
+        case 1:
+            result = num1 + num2;
+            printf("addition of two numbers is %d\n", result);
+            break;
+        case 2:
+            result = num1 - num2;
+            printf("subtraction of two numbers is %d\n", result);
+            break;
+        case 3:
+            result = num1 * num2;
+            printf("multiplication of two numbers is %d\n", result);
+            break;
+        case 4:
+            result = num1 / num2;
+            printf("division of two numbers is %d\n", result);
+            break;
+        default:
+            printf("wrong input\n");
+        }
+        printf("do you want to continue? y/n?\n");
+        scanf("%s", &option);
+    } while (option == 'y');
+    printf("Good bye");
     return 0;
 }
